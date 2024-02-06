944a791d746501b990de36e0b984144f597e6d65
ouaatyaa <ouaatyaa@gmail.com>
2024-02-06 12:21:02 +0100

    add confetti

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
-        <Toaster richColors />
+        <Toaster richColors position="top-center" />
       </body>
     </html>
   );
